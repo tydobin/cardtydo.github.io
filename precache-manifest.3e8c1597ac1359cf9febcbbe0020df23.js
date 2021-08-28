295c58a7a555b0d3cdeab58c9674c9c589e2c052
monsieurverde <41913050+monsieurverde@users.noreply.github.com>
2021-08-28 00:05:59 -0400

    first commit

---
+++ precache-manifest.3e8c1597ac1359cf9febcbbe0020df23.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d05694583131db424bd9ce0ff81bca9d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ee694c751000790b2de1",
+    "url": "/static/css/main.a835ff99.chunk.css"
+  },
+  {
+    "revision": "8ab5138c4b4cd90d41aa",
+    "url": "/static/js/2.2e5fb338.chunk.js"
+  },
+  {
+    "revision": "5b571a43117d7de158db9e221eaa0225",
+    "url": "/static/js/2.2e5fb338.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ee694c751000790b2de1",
+    "url": "/static/js/main.c481771d.chunk.js"
+  },
+  {
+    "revision": "784d9830d523aa7ea02f",
+    "url": "/static/js/runtime-main.49dfdc1c.js"
+  },
+  {
+    "revision": "2add42d83e2c487178d972c91e671d8a",
+    "url": "/static/media/IMG_20201124_104554.2add42d8.png"
+  },
+  {
+    "revision": "6fdab93bad40e6b81426cbfa4ae304eb",
+    "url": "/static/media/IMG_20201124_112759.6fdab93b.png"
+  },
+  {
+    "revision": "6cdffb93d1a794a9544bc4d4bd691a85",
+    "url": "/static/media/dribbble.6cdffb93.png"
+  },
+  {
+    "revision": "9dba69b0aecda4a97bbe567db7330823",
+    "url": "/static/media/github.9dba69b0.png"
+  },
+  {
+    "revision": "e025c5a4f2786f2592b469518e24a6b1",
+    "url": "/static/media/github2.e025c5a4.png"
+  },
+  {
+    "revision": "7ceed3640c0f5d56333d1658c6eb0bcd",
+    "url": "/static/media/instagram.7ceed364.png"
+  },
+  {
+    "revision": "b03d02108b54cb5aacfa601efa729c11",
+    "url": "/static/media/website.b03d0210.png"
+  },
+  {
+    "revision": "9bc34e1a64cd652c86a10d99aee40a46",
+    "url": "/static/media/youtube.9bc34e1a.png"
+  }
+]);
